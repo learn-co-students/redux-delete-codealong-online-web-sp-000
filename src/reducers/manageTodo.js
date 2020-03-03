12ae20371c1c9e1990d592884bf89f69e44a2a73
chanwkkk <chanwk18@hotmail.com>
2020-03-03 16:57:29 -0700

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,11 +1,21 @@
+import 'uuid'
+import uuid from 'uuid';
 export default function manageTodo(state = {
   todos: [],
 }, action) {
   console.log(action)
   switch (action.type) {
     case 'ADD_TODO':
+      const todo = {
+          id: uuid(),
+          text: action.payload.text
+      }
+      return { todos: state.todos.concat(todo) };
 
-      return { todos: state.todos.concat(action.payload.text) };
+    case 'DELETE_TODO':
+      const id = action.payload
+      return {todos: state.todos.filter(todo=>todo.id !== id)}
+    
 
     default:
       return state;
