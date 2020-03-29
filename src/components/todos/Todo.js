42d34482032c0bc130a819594441c29c42b6caf2
ch2isk4kos <ch2isk4kos@gmail.com>
2020-03-29 12:24:43 -0400

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,48 @@
 import React from 'react'
 
-const Todo = props => <li>{props.text}</li>
+// const Todo = props => <li>{props.text}</li>
+
+/* code change */
+const Todo = props => {
+    return (
+    <div>
+        {/* <span>{ props.text }</span><button onClick={() => props.delete(props.text)}>Delete</button> */}
+
+        {/* code change  */}
+        <span>{ props.todo.text }</span><button onClick={() => props.delete(props.todo.id)}>Delete</button>
+    </div>
+    )
+}
 
 export default Todo;
+
+/*
+This component is receiving `this.props.delete`.
+
+After applying the button, once it's clicked,
+you want to be able to delete this particular todo.
+
+At the moment, your todos are just strings, stored in an array.
+Since that is all you have to work with, add an onClick attribute to the new button.
+
+To keep this component small, you can provide an anonymous function in-line.
+
+You're now providing a definition for an anonymous function.
+
+Inside the definition, you're calling `props.delete`,
+and passing in the only other prop available, props.text.
+
+Back in your connected TodosContainer, when this delete button is clicked,
+the value of `props.text` is passed into your dispatched action as the `payload`.
+
+There is a console.log in your reducer that displays actions.
+Clicking the delete button should log an action with the todo's text content as the payload.
+
+Now you have the ability to dispatch an action to the reducer from each Todo.
+*/
+
+/*
+Now that you've got todo.id, you can modify the Todo component to use
+`props.todo.id` on click, so that when `props.delete `is called,
+an action is dispatched that contains an id only as its payload.
+*/
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,13 +1,71 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
-  console.log(action)
-  switch (action.type) {
-    case 'ADD_TODO':
+/* code change */
+import uuid from 'uuid'
 
-      return { todos: state.todos.concat(action.payload.text) };
+export default function manageTodo(state = {todos: [],}, action) {
+    console.log(action)
+
+    switch (action.type) {
+
+    case 'ADD_TODO':
+        // return { todos: state.todos.concat(action.payload.text) };
+
+        /* code change */
+        const todo = {
+            id: uuid(),
+            text: action.payload.text
+        }
+
+        /* code change */
+        return { todos: state.todos.concat(todo) };
+
+    /* code change */
+    case 'DELETE_TODO':
+        // return { todos: state.todos.filter(todo => todo !== action.payload) };
+        return { todos: state.todos.filter(todo => todo.id !== action.payload) };
 
     default:
-      return state;
-  }
+        return state;
+    }
 }
+
+/*
+Tell the store which Todo to Delete:
+
+Your todos are stored as strings in an array.
+
+There are a number of ways to remove a specific string from an array,
+but one of the more brief options is to use `filter()`.
+
+By adding a second case to your `manageTodo()` reducer,
+you can write a `filter()` that returns every todo that doesn't match
+what is contained in `action.payload`
+
+The delete button should now successful cause todos to disappear,
+but the problem is, what if you have multiple todos with the same text?
+
+With this set up, every todo that matches `action.payload` will be filtered out.
+
+To get around this, instead of filtering just text,
+gave your `Todos` specific IDs using the `uuid` library.
+
+A Todo should have an id the moment it gets created.
+
+Since your reducer creates the Todo when a `CREATE_TODO` action is dispatched,
+update the code so that it also adds an id.
+
+Using `uuid()` will generate a long random string each time a todo is created.
+
+Now, instead of just storing an array of strings in your store,
+you'll be storing an array of objects.
+
+This causes a problem 'downstream', though:
+you need to update your `TodosContainer` to pass the correct content.
+*/
+
+/*
+After passing an id to `props.delete`, you need to modify the reducer so that
+instead of comparing `todo !== action.payload`, now that todo is an object,
+and you want to match todo.id with the payload.
+
+That way, the todo objects can be added and deleted, each with their own unique id!
+*/
