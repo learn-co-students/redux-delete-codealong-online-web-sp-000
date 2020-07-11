1dfa9bf282924e9d142fa3124bdc1cffd6f7558e
StephanieAlyssa <alyssa.yesilyurt@gmail.com>
2020-07-11 00:36:42 -0400

    Done.

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -1,24 +1,34 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import Todo from './Todo'
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+    renderTodos = () => {
+        return this.props.todos.map(todo => <Todo delete={this.props.delete} key={todo.id} todo={todo} />)    
+    }
+    //Each component rendered now has access to the delete action
 
-  render() {
-    return(
-      <div>
-        {this.renderTodos()}
-      </div>
-    );
-  }
+    render() {
+        return(
+            <div>
+                {this.renderTodos()}
+            </div>
+        );
+    }
 };
 
 const mapStateToProps = state => {
-  return {
-    todos: state.todos
-  }
+    return {
+        todos: state.todos
+    }
 }
 
-export default connect(mapStateToProps)(TodosContainer);
+const mapDispatchToProps = dispatch => {
+    //TodosContainer now has access to this.props.delete which can take in an argument and send it as the action's payload. We can then pass this.props.delete down to Todo so that each Todo component rendered will have access to our DELETE_TODO action
+    return {
+        delete: todoText => dispatch({type: 'DELETE_TODO', payload: todoText})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
