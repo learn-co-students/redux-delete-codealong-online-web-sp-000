1a70cf52375cb56e33d652679e460f498f929b8a
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-10-19 19:06:18 +0100

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,30 @@
 import React from 'react'
 
-const Todo = props => <li>{props.text}</li>
+// const Todo = props => <li>{props.text}</li>
+
+// Todo is receiving this.props.delete
+const Todo = props => {
+    return (
+        <div>
+            {/* <span>{props.text}</span> */}
+            {/* when id is added */}
+            <span>{props.todo.text}</span>
+            {/* <button onClick={() => props.delete(props.text)}>DELETE</button> */}
+            {/* Now that we've got todo.id, we can modify the Todo component to use props.todo.id on click */}
+            <button onClick={() => props.delete(props.todo.id)}>DELETE</button>        
+        </div>
+    )
+}
 
 export default Todo;
+
+// When we click the button we want to be able to delete this particular todo
+//  we add an onClick attribute to the new button
+// To keep this component small, we can provide an anonymous function in-line
+// We're providing a definition for an anonymous function
+//  Inside the definition, we're calling props.delete , and passing in the only other prop available, props.text
+
+// Back in our connected TodosContainer, when this delete button is clicked
+// the value of props.text is passed into our dispatched action as the payload.
+// now we have the ability to dispatch an action to the reducer from each Todo
+
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,3 +1,5 @@
+import uuid from 'uuid';
+
 export default function manageTodo(state = {
   todos: [],
 }, action) {
@@ -5,9 +7,39 @@ export default function manageTodo(state = {
   switch (action.type) {
     case 'ADD_TODO':
 
-      return { todos: state.todos.concat(action.payload.text) };
+      // return { todos: state.todos.concat(action.payload.text) };
+
+      const todo = {
+        id: uuid(),
+        text: action.payload.text
+      }
+
+      return { todos: state.todos.concat(todo) };
+    
+    case 'DELETE_TODO':
+
+    //  return{ todos: state.todos.filter(todo => todo !== action.payload) }
+      //  that returns every todo that doesn't match what is contained in action.payload
+      // wnen id is added as uuid then return is - i to ne radi???
+      return {todos: state.todos.filter(todo => todo.id !== action.payload)}
 
     default:
       return state;
   }
 }
+
+// Our todos are stored as strings in an array
+// to remove a specific string from an array, but one of the more brief options is to use filter
+//  By adding a second case to our manageTodo reducer, we can write a filter
+//  that returns every todo that doesn't match what is contained in action.payload
+
+// What if you have multiple todos with the same text? 
+// With this set up, every todo that matches action.payload will be filtered out
+// instead of filtering just text, it would be better if we gave our Todos specific IDs
+
+// A Todo should have an id the moment it gets created. 
+// So, we know that our reducer creates the Todo when a ADD_TODO action is dispatched.
+//  Let's update the code in there so that it also adds an id.
+// Using uuid() will generate a long random string each time a todo is created.
+// Now, instead of just storing an array of strings in our store, we'll be storing an array of objects.
+
