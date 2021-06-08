1d0b29f526521f36cb70e31df0a743b36f8802d5
A.J. Romaniello <ajiellodev@gmail.com>
2021-06-08 10:39:33 -0600

    Done.

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -4,16 +4,17 @@ import Todo from './Todo'
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+  renderTodos = () => this.props.todos
+      .map((todo, id) => <Todo key={id} todo={todo} delete={this.props.delete}/>)
 
   render() {
-    return(
-      <div>
-        {this.renderTodos()}
-      </div>
+    return (
+        <ul>
+          {this.renderTodos()}
+        </ul>
     );
   }
-};
+}
 
 const mapStateToProps = state => {
   return {
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,12 +1,14 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
-  console.log(action)
+import uuid from 'uuid'
+
+export default function manageTodo(state = { todos: [] }, action) {
+  console.log('State', state, 'Action', action)
   switch (action.type) {
     case 'ADD_TODO':
-
-      return { todos: state.todos.concat(action.payload.text) };
-
+      const TODO = { id: uuid(), text: action.payload.text }
+        console.log('NEW TODO', TODO)
+      return { todos: state.todos.concat(TODO) };
+    case 'DELETE_TODO':
+      return { todos: state.todos.filter(todo => todo.id !== action.payload)}
     default:
       return state;
   }
